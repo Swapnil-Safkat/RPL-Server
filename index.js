a8cb7e99dac54d47343bc0f9546f29b897ad831f
Swapnil-Safkat <shahriarswapnil50@gmail.com>
2022-10-18 01:23:10 +0600

    versel setup

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
 import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER
   }:${process.env.DB_PASS
